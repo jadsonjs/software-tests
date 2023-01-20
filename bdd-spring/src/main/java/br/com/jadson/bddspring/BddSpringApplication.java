cf18249803606ef988e0dcf0bd879e257b1db437
Jadson Santos <jadson.santos@ufrn.br>
2023-01-20 10:18:48 -0300

    bdd wih cucumber - spring
    Signed-off-by: Jadson Santos <jadson.santos@ufrn.br>

---
+++ bdd-spring/src/main/java/br/com/jadson/bddspring/BddSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.jadson.bddspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddSpringApplication.class, args);
+	}
+
+}
---
+++ bdd-spring/src/test/java/br/com/jadson/bddspring/BddSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.jadson.bddspring;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BddSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
